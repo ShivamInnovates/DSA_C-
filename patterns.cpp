5d02b01b12767118e5db3218066029767b10d0f1
Shivam Potpalliwar <shivam.potpalliwar@gmail.com>
2025-08-20 13:22:30 +0530

    Patter 1: 4*4 star by while

--- patterns.cpp
+++ patterns.cpp
@@ -2,12 +2,23 @@
 using namespace std;
 
 int main() {
-    for(int i =0; i<4; i++)
-    {
-        for (int j = 0; j<4; j++)
-        {
-            cout<<"*    ";
+    // for(int i =0; i<4; i++)
+    // {
+    //     for (int j = 0; j<4; j++)
+    //     {
+    //         cout<<"*    ";
+    //     }
+    //     cout<<"\n";
+    // }
+
+    int a = 4;
+    while(a > 0){
+        int b = 4;
+        while (b > 0){
+            cout <<"* ";
+            b--;
         }
+        a--;
         cout<<"\n";
     }
 
