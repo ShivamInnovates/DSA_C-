6540dcae730162e346c9341c8a68ca2db90f47ce
Shivam Potpalliwar <shivam.potpalliwar@gmail.com>
2025-08-20 12:53:33 +0530

    basic If_else

--- basic.cpp
+++ basic.cpp
@@ -3,8 +3,27 @@ using namespace std;
 
 int main()
 {   string nm;
-    cout<<"Enter the name: ";
-    cin>>nm;
-    cout<<"My name is "<<nm;
+    char ch;
+    // cout<<"Enter the name: ";
+    // cin>>nm;
+    // cout<<"My name is "<<nm;
 
+    cout<<"Character accept : ";
+    cin>>ch;
+
+    if(ch>='A' && ch<='Z')
+    {
+        cout<<"Uppercase letter";
+    }
+    else if(ch>='a' && ch<='z')
+    {
+        cout<<"Lowercase letter";
+    }
+    else if(ch>='0' && ch<='9')
+    {
+        cout<<"Numeric";
+    }
+    else{
+        cout<<"Special characters";
+    }
 }
