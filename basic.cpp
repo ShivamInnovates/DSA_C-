7c79c22a77ac1460cdc6f17d6125700b2a5066be
Shivam Potpalliwar <shivam.potpalliwar@gmail.com>
2025-08-20 13:02:47 +0530

    prime numbers

--- basic.cpp
+++ basic.cpp
@@ -1,29 +1,46 @@
 #include<iostream>
 using namespace std;
 
-int main()
-{   string nm;
-    char ch;
-    // cout<<"Enter the name: ";
-    // cin>>nm;
-    // cout<<"My name is "<<nm;
+int main(){
+//    string nm;
+//     char ch;
+//     cout<<"Enter the name: ";
+//     cin>>nm;
+//     cout<<"My name is "<<nm;
 
-    cout<<"Character accept : ";
-    cin>>ch;
+//     cout<<"Character accept : ";
+//     cin>>ch;
 
-    if(ch>='A' && ch<='Z')
+//     if(ch>='A' && ch<='Z')
+//     {
+//         cout<<"Uppercase letter";
+//     }
+//     else if(ch>='a' && ch<='z')
+//     {
+//         cout<<"Lowercase letter";
+//     }
+//     else if(ch>='0' && ch<='9')
+//     {
+//         cout<<"Numeric";
+//     }
+//     else{
+//         cout<<"Special characters";
+//     }
+    int num;
+    int flag =0;
+    cout<<"Enter the number: ";
+    cin>>num;
+    for(int i = 2 ; i<num; i++)
     {
-        cout<<"Uppercase letter";
+        if(num%i==0)
+        {
+            flag=1;
+            break;
+        }
     }
-    else if(ch>='a' && ch<='z')
-    {
-        cout<<"Lowercase letter";
-    }
-    else if(ch>='0' && ch<='9')
-    {
-        cout<<"Numeric";
-    }
-    else{
-        cout<<"Special characters";
-    }
+
+    if (flag == 0) 
+        cout<<"Prime number";
+    else 
+        cout<<"Not Prime";
 }
