a14f6aad439a3813f6402d91e098817f8d2a9190
Shivam Potpalliwar <shivam.potpalliwar@gmail.com>
2025-08-20 13:12:44 +0530

    Patter 1: 4*4 star

--- basic.cpp
+++ basic.cpp
@@ -2,30 +2,30 @@
 using namespace std;
 
 int main(){
-//    string nm;
-//     char ch;
-//     cout<<"Enter the name: ";
-//     cin>>nm;
-//     cout<<"My name is "<<nm;
+   string nm;
+    char ch;
+    cout<<"Enter the name: ";
+    cin>>nm;
+    cout<<"My name is "<<nm;
 
-//     cout<<"Character accept : ";
-//     cin>>ch;
+    cout<<"Character accept : ";
+    cin>>ch;
 
-//     if(ch>='A' && ch<='Z')
-//     {
-//         cout<<"Uppercase letter";
-//     }
-//     else if(ch>='a' && ch<='z')
-//     {
-//         cout<<"Lowercase letter";
-//     }
-//     else if(ch>='0' && ch<='9')
-//     {
-//         cout<<"Numeric";
-//     }
-//     else{
-//         cout<<"Special characters";
-//     }
+    if(ch>='A' && ch<='Z')
+    {
+        cout<<"Uppercase letter";
+    }
+    else if(ch>='a' && ch<='z')
+    {
+        cout<<"Lowercase letter";
+    }
+    else if(ch>='0' && ch<='9')
+    {
+        cout<<"Numeric";
+    }
+    else{
+        cout<<"Special characters";
+    }
     int num;
     int flag =0;
     cout<<"Enter the number: ";
