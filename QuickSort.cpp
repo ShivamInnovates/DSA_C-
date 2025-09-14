961604637500fdbe3cd615aa9cfdc2bae50b031f
Shivam Potpalliwar <shivam.potpalliwar@gmail.com>
2025-09-14 17:52:21 +0530

    Quick Sort

---
+++ QuickSort.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int partition(int arr[], int low, int high)
+{
+    int pivot = arr[low];
+    int i = low;
+    int j = high;
+
+    while(i<j)
+    {
+        while(arr[i]<=pivot && i<=high) i++;
+
+        while(arr[j]>pivot && j>=low) j--;
+
+        if(i<j) swap(arr[i], arr[j]);
+    }
+    swap(arr[low],arr[j]);
+    return j;
+}
+void quickSort(int arr[], int low, int high)
+{
+    if(low<high)
+    {
+        int part = partition(arr, low, high);
+        quickSort(arr, low, part-1);
+        quickSort(arr, part+1, high);
+    }
+}
+int main()
+{
+    int arr[] = {64,25,12,22,11};
+    int n = size(arr);
+    int low =0, high =n-1;
+    quickSort(arr, low, high);
+    for(int i : arr)
+    {
+        cout<<i<<" ";
+    }
+    return 0;
+}
